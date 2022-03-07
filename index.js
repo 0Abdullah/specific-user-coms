1db35fa633783c948eaf2389d4d8e935b40f9c88
Abdullah <78501546+0Abdullah@users.noreply.github.com>
2022-03-08 02:56:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/user/:requiredUser/:username', (req, res) => {
-    res.send(req.params.username.toLowerCase() == req.params.requiredUser.toLowerCase() ? 'x3waasItti x3waasItti' : ' ')
+    res.send(req.params.username.toLowerCase() == req.params.requiredUser.toLowerCase() ? 'x3waasItti x3waasItti' : `دز وين غيده تربيك؟ @${req.params.username}`)
 });;
 
 app.listen(port, () => {
