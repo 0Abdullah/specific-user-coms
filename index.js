2a35cba4ce736b2a5617b0674a8f1b0874dc703b
Abdullah <78501546+0Abdullah@users.noreply.github.com>
2022-03-17 13:04:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let port = process.env.PORT || 3000
 const express = require('express');
 const app = express();
 
-app.get('/user/:requiredUser/:username/:msg', (req, res) => {
+app.get('/user/:requiredUser/:username/:msg/:badMsg', (req, res) => {
     if(req.params.username.toLowerCase() == req.params.requiredUser.toLowerCase()) {
         res.send(req.params.msg)
     } else if(req.params.username.toLowerCase() == '3bdullahvv') {
         res.send('@3bdullahVv حبيبي')
     } else {
-        res.send(`@${req.params.username} انقلع`)
+        res.send(req.params.badMsg)
     }
     //res.send(req.params.username.toLowerCase() == req.params.requiredUser.toLowerCase() ? 'x3waasItti x3waasItti' : `@${req.params.username} انقلع`)
 });;
